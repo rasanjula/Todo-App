d96708f38ed52c9d65864001df34b392f030f2c8
rasanjula <rasanjula89@gmail.com>
2025-03-21 10:15:25 +0200

    added  aprivate method and a public method

--- js/class/Todos.js
+++ js/class/Todos.js
@@ -1,36 +1,62 @@
-import { Task } from "./Task.js";  // Import Task class
+import { Task } from './Task.js';
 
 class Todos {
   #tasks = [];
   #backend_url = '';
 
-  // Constructor takes the backend URL as a parameter
   constructor(url) {
     this.#backend_url = url;
   }
+
+  // Private method to add task to tasks array
+  #addToArray = (id, text) => {
+    const task = new Task(id, text);  // Create a new Task object
+    this.#tasks.push(task);  // Add the task to the tasks array
+    return task;  // Return the newly added task
+  };
 
   // Method to fetch tasks from the backend
   getTasks = () => {
     return new Promise(async (resolve, reject) => {
       fetch(this.#backend_url)
-        .then((response) => response.json()) // Parse the JSON response
+        .then((response) => response.json())
         .then((json) => {
-          this.#readJson(json);  // Call private method to read JSON data
-          resolve(this.#tasks);   // Resolve promise with tasks
+          this.#readJson(json);  // Call private method to read JSON
+          resolve(this.#tasks);  // Return the tasks array
         })
-        .catch((error) => {
-          reject(error); // Reject promise with error if fetch fails
-        });
+        .catch((error) => reject(error));
     });
   };
 
-  // Private method to process the JSON response and populate tasks array
+  // Private method to read JSON into the tasks array
   #readJson = (tasksAsJson) => {
     tasksAsJson.forEach((node) => {
-      const task = new Task(node.id, node.description);  // Create Task objects
-      this.#tasks.push(task);  // Add task to tasks array
+      const task = new Task(node.id, node.description);
+      this.#tasks.push(task);  // Push each task to the tasks array
+    });
+  };
+
+  // Public method to add a new task
+  addTask = (text) => {
+    return new Promise((resolve, reject) => {
+      const json = JSON.stringify({ description: text });  // Convert description to JSON
+
+      fetch(this.#backend_url + '/new', {
+        method: 'POST',  // Use POST HTTP method
+        headers: {
+          'Content-Type': 'application/json',  // Send data as JSON
+        },
+        body: json,  // Send the task description in the request body
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          // Add the task to the tasks array after receiving response
+          const task = this.#addToArray(json.id, json.description);
+          resolve(task);  // Return the added task
+        })
+        .catch((error) => reject(error));  // Handle any errors
     });
   };
 }
 
-export { Todos };  // Export the Todos class to be used in other files
+export { Todos };
